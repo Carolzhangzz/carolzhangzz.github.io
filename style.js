7aad96c99532f1a485df9f26c43f3b9bd9e933c7
carol_zhang <carolzhang705@gmail.com>
2024-10-07 02:24:32 -0700

    update

--- style.js
+++ style.js
@@ -16,23 +16,5 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-async function updateStarCount() {
-    const repo = 'Carolzhangzz'; // 替换成你的 GitHub 仓库
-    const url = `https://api.github.com/repos/${repo}`;
-    
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        
-        if (data.stargazers_count !== undefined) {
-            document.getElementById('star-count').textContent = `★ ${data.stargazers_count}`;
-        } else {
-            console.error('Error fetching stargazer count:', data);
-        }
-    } catch (error) {
-        console.error('Error fetching repository data:', error);
-    }
-}
 
-// 页面加载时更新星星数
-document.addEventListener('DOMContentLoaded', updateStarCount);
+
